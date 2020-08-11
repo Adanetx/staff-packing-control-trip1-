3969a726572e35fc38574a20aba58f4c2713bc68
Adanetx <Adane1301992@gmail.com>
2020-08-11 10:47:46 -0500

    fix

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://staff-packing-control-trip.herokuapp.com/',
+  production: 'https://staff-packing-control-trip.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
